dbe9273be5d442054f309ad1282d350b3e8389ac
DungHoang0411 <Dung042006@gmail.com>
2025-07-30 13:35:51 +0700

    delete slider.js and update index

--- slider.js
+++
@@ -1,9 +0,0 @@
-// slider.js
-document.addEventListener('DOMContentLoaded', () => {
-    new Swiper('.mySwiper', {
-        loop: true,
-        autoplay: { delay: 2500 },
-        pagination: { el: '.swiper-pagination', clickable: true },
-        navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-    });
-});
